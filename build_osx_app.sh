c824ae9d9022ab36e8b865b152b4b2cbe8bf12d7
Adam Parrish <adam@decontextualize.com>
2011-02-17 10:49:35 -0500

    revised build scripts

--- build_osx_app.sh
+++ build_osx_app.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-rm -rf build/Characterror.app
-cp -r build_templates/osx_template.app build/Characterror.app
-cp -r processing-py.jar game.py gamestate.py lettertree.py libraries tweener.py wordlist_short data build/Characterror.app/Contents/Resources/Java
+rm -rf build/osx
+mkdir -p build/osx/Characterror
+cp -r build_templates/osx_template.app build/osx/Characterror/Characterror.app
+cp -r processing-py.jar game.py gamestate.py lettertree.py libraries tweener.py wordlist_short data build/osx/Characterror/Characterror.app/Contents/Resources/Java
+cp -r README build/osx/Characterror
+pushd build/osx
+zip -r Characterror-osx.zip Characterror/*
+popd
--- build_windows_exe.sh
+++ build_windows_exe.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 LAUNCH4JPATH=/Users/adam/Downloads/launch4j
-rm -rf build/Characterror.exe
-rm -rf build/resources
-mkdir build/resources
-cp -r processing-py.jar game.py gamestate.py lettertree.py libraries tweener.py wordlist_short data build/resources
+rm -rf build/win
+mkdir -p build/win/Characterror/resources
+cp -r processing-py.jar game.py gamestate.py lettertree.py libraries tweener.py wordlist_short data build/win/Characterror/resources
+cp -r README build/win/Characterror
 java -jar $LAUNCH4JPATH/launch4j.jar l4j.xml
+pushd build/win
+zip -r Characterror-win.zip Characterror/*
+popd
